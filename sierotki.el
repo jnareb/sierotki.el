0137cebc010cef1a00e7a7a3318dbea378f66f18
Jakub Narebski <jnareb@gmail.com>
2002-11-15 01:20:19 +0000

    tex-hard-spaces: Używa regexp zawartego w zmiennej tex-hard-spaces-regexp tex-magic-space: Używa regexp zawartego w zmiennej tex-magic-space-regexp
    tex-toggle-magic-space: Użycie bieżącej lokalnej mapy klawiatury lub
    bieżącej globalnej mapy.

--- sierotki.el
+++ sierotki.el
@@ -6,7 +6,7 @@
 ;;	Jakub Narebski <jnareb@fuw.edu.pl>
 ;;	Adam P. <adamp_at@at_ipipan.waw.pl>
 ;; Maintainer: Jakub Narebski <jnareb@fuw.edu.pl>
-;; Version: 1.2
+;; Version: 1.3
 ;; Keywords: tex, wp
 ;; Created: 03-11-1999
 
@@ -25,24 +25,39 @@
 
 ;; Introduce (interactively) tilde characters (\TeX's hard-space ~)
 ;; after one-letter Polish articles in the entire buffer.
+;; Poni¿sza zmienna definiuje wyra¿enie regularne u¿ywane w `tex-hard-spaces'
+(defvar tex-hard-spaces-regexp "\\<\\([aeiouwzAEIOUWZ]\\)\\s +"
+  "*Regular expression which detects single [aeiouwz] for `tex-hard-spaces'.
+The part of regular expression which matches string to be saved 
+should be in parentheses, so the replace part \\\\1~ will work.
+
+Used as first argument to `query-replace-regexp'.")
+
 ;; Zwyk³e `query-replace-regexp', czyli C-M-% dla odpowiednich wyra¿eñ regularnych
 (defun tex-hard-spaces ()
   "Replaces space after single-letter word with '~', 
 the TeX non-breakable space"
  (interactive)
- (query-replace-regexp "\\<\\([wiazouWIAZOU]\\)\\s +"
+ (query-replace-regexp tex-hard-spaces-regexp
                        "\\1~"))
 
 
 ;;;; ======================================================================
 ;;;; Zapobieganie powstawaniu sierotek 'w locie'
 
+(defvar tex-magic-space-regexp "\\<[aeiouwzAEIOUWZ]\\'"
+  "*Regular expression which detects single [aeiouwz] for `tex-magic-space'.
+This regular expression should end with \\\\' to match against \"point\",
+and begin with something matching against word boundary.
+
+Used in comparing with part of buffer before point.")
+
 ;;; Magic space by Michal Jankowski <michalj@fuw.edu.pl>
 (defun tex-magic-space () 
   "Magic-space - inserts non-breakable space after a single-letter word." 
   (interactive)
   (if (string-match 
-       "\\<[aeiouwzAEIOUWZ]\\'" 
+       tex-magic-space-regexp
        (buffer-substring (max (point-min) (- (point) 2)) (point)))
       (insert "~")
     (insert " ")
@@ -58,11 +73,16 @@ the TeX non-breakable space"
   "Toggles TeX magic space, which inserts non-breakable space after a
 single-letter word"
   (interactive)
-  (progn
-    (if (local-key-binding " " 'tex-magic-space)
-	(local-unset-key " ")
-      (local-set-key " " tex-magic-space))
-    (describe-key-briefly " ")))
+  (let 
+    ((map (or (current-local-map)
+              (current-global-map))))
+    (progn
+      (if (equal (lookup-key map " ") 'tex-magic-space)
+          (progn
+            (define-key map " " nil)
+            (local-unset-key " ")) ; to be sure
+        (define-key map " " 'tex-magic-space))
+    (describe-key-briefly " "))))
 
 
 ;;; ---------------------------------------------------------------------
