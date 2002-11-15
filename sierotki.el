d17acb0fb370d7631952ff6ecdd4b93ed507ac34
Jakub Narebski <jnareb@gmail.com>
2002-11-15 01:02:39 +0000

    tex-magic-space: Zmieniony regexp. tex-toggle-magic-space: Użycie lokalnej mapy zamiast LaTeX-mode-map,   użycie describe-key-briefly do pokazania rezultatu.
    Dopisana data "Created:" na podstawie zapisanego artykułu z
    pl.comp.dtp.tex, gdzie ten kod pojawił się po raz pierwszy.
    
    Dodane i zmienione komentarze.
    
    Inicjalizacja dla RefTeX-a zmieniona z hook na eval-after-load.

--- sierotki.el
+++ sierotki.el
@@ -6,9 +6,9 @@
 ;;	Jakub Narebski <jnareb@fuw.edu.pl>
 ;;	Adam P. <adamp_at@at_ipipan.waw.pl>
 ;; Maintainer: Jakub Narebski <jnareb@fuw.edu.pl>
-;; Version: 1.0
+;; Version: 1.2
 ;; Keywords: tex, wp
-;; Created: 
+;; Created: 03-11-1999
 
 ;; $Id$
 
@@ -16,10 +16,16 @@
 
 ;;; Code:
 
-;;; ---------------------------------------------------------------------- 
-;;; From: rysiek@IPIPAN.GDA.PL (Ryszard Kubiak)
+
+;;;; ======================================================================
+;;;; Usuwanie sierotek w istniej±cym dokumencie, interaktywne.
+
+;;; Hard spaces by Ryszard Kubiak <rysiek@ipipan.gda.pl>
+;;; Modified by Jakub Narêbski <jnareb@fuw.edu.pl>
+
 ;; Introduce (interactively) tilde characters (\TeX's hard-space ~)
-;; after one-letter Polish articles in the entire buffer
+;; after one-letter Polish articles in the entire buffer.
+;; Zwyk³e `query-replace-regexp', czyli C-M-% dla odpowiednich wyra¿eñ regularnych
 (defun tex-hard-spaces ()
   "Replaces space after single-letter word with '~', 
 the TeX non-breakable space"
@@ -27,48 +33,58 @@ the TeX non-breakable space"
  (query-replace-regexp "\\<\\([wiazouWIAZOU]\\)\\s +"
                        "\\1~"))
 
-;;; ---------------------------------------------------------------------- 
+
+;;;; ======================================================================
+;;;; Zapobieganie powstawaniu sierotek 'w locie'
+
 ;;; Magic space by Michal Jankowski <michalj@fuw.edu.pl>
 (defun tex-magic-space () 
   "Magic-space - inserts non-breakable space after a single-letter word." 
   (interactive)
   (if (string-match 
-       "^\\(\\s \\|~\\)?[aeiouwz]$" 
+       "\\<[aeiouwzAEIOUWZ]\\'" 
        (buffer-substring (max (point-min) (- (point) 2)) (point)))
       (insert "~")
     (insert " ")
     (and auto-fill-function
 	 (funcall auto-fill-function))))
 
-;; 'Rêcznie' mo¿na j± w³±czyæ za pomoc±
-;; M-x local-set-key SPC tex-magic-space
-;; albo (obecnie) za pomoc± C-c SPC.
-;; UWAGA: Dzia³a gdy u¿ywamy AUCTeX-a; dla latex-mode nale¿y zmieniæ
-;;        TeX-mode-map na tex-mode-map
+;;; ----------------------------------------------------------------------
+;;; Toggle magic space by Jakub Narêbski <jnareb@fuw.edu.pl>, 
+
+;; Przypisuje/wy³±cza przypisanie tex-magic-space do spacji,
+;; (przydatne przy pisaniu matematyki), [tylko dla trybów LaTeX-owych]
 (defun tex-toggle-magic-space ()
   "Toggles TeX magic space, which inserts non-breakable space after a
 single-letter word"
   (interactive)
   (progn
-    (if (equal (lookup-key TeX-mode-map " ") 'tex-magic-space)
-        (progn
-          (define-key TeX-mode-map " " nil)
-          (local-unset-key " ")) ; to be sure
-      (define-key TeX-mode-map " " 'tex-magic-space))
-    (message "SPC is binded to %s" (lookup-key TeX-mode-map " "))))
+    (if (local-key-binding " " 'tex-magic-space)
+	(local-unset-key " ")
+      (local-set-key " " tex-magic-space))
+    (describe-key-briefly " ")))
+
+
+;;; ---------------------------------------------------------------------
+;;; Inicjalizacja dla zapobiegania powstawaniu sierotek 'w locie'
+
+;;; Initialization, by Jakub Narêbski <jnareb@fuw.edu.pl> 
+;;; and Adam P. <adamp_at@at_ipipan.waw.pl>
+
 ;; C-c SPC toggles magic space:
-;; mode-specific-map is keymap for characters following C-c
-;;; From: adamp_at@at_ipipan.waw.pl (Adam P.)
+;; `mode-specific-map' is keymap for characters following C-c
+;; Sequences consisting of `C-c' followed by any punctuation character 
+;; other than `{', `}', `<', `>', `:', `;' are allocated for minor modes.
+;; by Adam P. <adamp_at@at_ipipan.waw.pl>
 (define-key mode-specific-map " " 'tex-toggle-magic-space) ; C-c SPC
 
+;;; Initialize SPC to `tex-magic-space' using eval-after-load
 ;; For AUC TeX
-(eval-after-load "tex" '(define-key TeX-mode-map " " 'tex-magic-space))
+(eval-after-load "tex"      '(define-key TeX-mode-map    " " 'tex-magic-space))
+(eval-after-load "latex"    '(define-key LaTeX-mode-map  " " 'tex-magic-space))
 ;; For tex-mode as included in Emacs
-(eval-after-load "tex-mode" '(define-key tex-mode-map " " 'tex-magic-space))
+(eval-after-load "tex-mode" '(define-key tex-mode-map    " " 'tex-magic-space))
 ;; For RefTeX
-(add-hook 'reftex-mode-hook
-          (function
-           (lambda ()
-              (define-key reftex-mode-map " " 'tex-magic-space))))
+(eval-after-load "reftex"   '(define-key reftex-mode-map " " 'tex-magic-space))
 
 ;;; sierotki.el ends here
